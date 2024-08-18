0bbe7528d1318ae3d7f476f7d98c77b34dd20c50
Abdelrahman-Adel610 <engba80818233@gmail.com>
2024-08-18 14:57:16 +0300

    Added the game logic and With some interface updates

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 "use strict";
+/**********BUTTONS**********/
+let againBtn = document.querySelector(".again-btn");
+let checkBtn = document.querySelector(".check-btn");
+/**********STATUS**********/
+let displayNumber = document.querySelector(".ques-mark");
+let message = document.querySelector(".message");
+let input = document.querySelector("input");
+/**********SCORE**********/
+let score = document.querySelector(".score");
+let highScore = document.querySelector(".highscore");
+/**********VARIABLES**********/
+let target = Math.trunc(Math.random() * 20) + 1;
+let maxScore = 0;
+let trials = 20;
+/*******************************************/
+/****************UTILITIES*****************/
+function checkTrial() {
+  return +input.value - target;
+}
+function updateInertface(trial) {
+  displayNumber.textContent = input.value;
+  if (trial === 0) {
+    message.textContent = "🎉 Correct number";
+    maxScore = Math.max(maxScore, trials);
+    highScore.textContent = maxScore;
+  } else if (trial < 0) {
+    message.textContent = "📉 Too low";
+    trials--;
+  } else {
+    message.textContent = "📈 Too high";
+    trials--;
+  }
+  score.textContent = trials;
+}
+function reset() {
+  target = Math.trunc(Math.random() * 20) + 1;
+//   maxScore = Math.max(maxScore, trials);
+  trials = 20;
+  input.value = "";
+  displayNumber.textContent = "?";
+  message.textContent = "Start guessing...";
+  highScore.textContent = maxScore;
+  score.textContent = trials;
+}
+/*****************************************/
+checkBtn.addEventListener("click", function () {
+  if (!input.value) {
+    message.textContent = "⛔ No input yet";
+  } else {
+    updateInertface(checkTrial());
+  }
+});
+againBtn.addEventListener("click", function () {
+  reset();
+});
